7736b68aa84c67dcf57d40dc3ebd16ea4bcd6a80
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-03-28 17:40:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -12,5 +12,21 @@ recognition.onresult = function (event) {
     var content = event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textarea").innerHTML = content;
+    speak();
 }
 
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textarea").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
